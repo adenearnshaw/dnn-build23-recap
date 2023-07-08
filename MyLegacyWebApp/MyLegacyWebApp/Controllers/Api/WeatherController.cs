5427d4acca362bcc0044491f62819abeed17bab1
Aden.Earnshaw <aden.earnshaw@positive-solutions.co.uk>
2023-07-08 09:58:02 +0100

    Adding Weather API

---
+++ MyLegacyWebApp/MyLegacyWebApp/Controllers/Api/WeatherController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyLegacyWebApp.Controllers.Api
+{
+    public class WeatherController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Cloudy", "Overcast" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "Dull";
+        }
+    }
+}
